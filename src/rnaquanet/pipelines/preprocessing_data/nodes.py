dc5208109b15dab592ac7a9f2a0f2b38c801a933
Angelfrost <64253119+Angelfrost@users.noreply.github.com>
2023-05-22 13:42:58 +0200

    Add progress bar for filtering files

--- src/rnaquanet/pipelines/preprocessing_data/nodes.py
+++ src/rnaquanet/pipelines/preprocessing_data/nodes.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import shutil
+from tqdm import tqdm
 
 def prepare_catalogs(src_dir: str, dest_dir: str, mappings: list[dict]) -> str:
     """
@@ -38,6 +39,10 @@ def filter_files(src: str, dest: str) -> None:
     pattern = f'{src}/**/*.pdb'
     src_files = glob.glob(pattern, recursive=True)
 
+    # Progress bar
+    total_files = len(src_files)+1
+    progress_bar = tqdm(total=total_files, unit='f')
+
     for in_filename in src_files:
         name, ext = os.path.splitext(in_filename)
         out_filename = f'{name}_filtered{ext}'.replace(src, dest)
@@ -51,5 +56,9 @@ def filter_files(src: str, dest: str) -> None:
                 for line in input_file:
                     if line.startswith('ATOM') or line.startswith('TER'):
                         output_file.write(line)
+        
+        progress_bar.update()
     
     shutil.move(f'{src}/scores.sc', f'{dest}/scores.sc')
+    progress_bar.update()
+    progress_bar.close()
