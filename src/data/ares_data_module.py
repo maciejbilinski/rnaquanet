9729ded915b8f4c0f78e41760b09240988d09c16
maciejbilinski <maciej.bilinski.programming@gmail.com>
2023-11-24 12:05:11 +0000

    Old experiments

--- src/data/ares_data_module.py
+++ src/data/ares_data_module.py
@@ -4,7 +4,7 @@ from torch_geometric.loader import DataLoader
 from .preprocess_utils import load_data_from_hdf5
 
 class AresDataModule(pl.LightningDataModule):
-    def __init__(self, config, batch_size=2000, num_workers=1, shuffle_train=True, shuffle_val=False):
+    def __init__(self, config, batch_size=1000, num_workers=1, shuffle_train=True, shuffle_val=False):
         super().__init__()
         self.config = config
         self.batch_size = batch_size
@@ -24,3 +24,6 @@ class AresDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.train_data[:4000], batch_size=self.batch_size, shuffle=self.shuffle_val, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=self.shuffle_val, num_workers=self.num_workers)
