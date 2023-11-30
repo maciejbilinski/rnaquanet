8dab81a069c78663353d3486a9b95f038cc862f3
Szymon Stanisławski <szymol1001@gmail.com>
2023-11-30 21:14:56 +0100

    #42 add working prototype

--- API/scripts/process_files.py
+++ API/scripts/process_files.py
@@ -28,9 +28,24 @@ def process_files(files: ImmutableMultiDict[str, FileStorage], task_id: str):
             filepath = os.path.join(files_dir_path, secure_filename(file.filename))
             file.save(filepath)
         
-        print("almost testing")
-        # TODO process saved files in a seperate process/thread
-        Thread(target=test, args=[task_id]).start()  # TODO remove, it's temp for testing
+        # TODO remove the dummy output thread and instead execute a command to process saved files
+        # after files are processed, write to their corresponding `status.json` file in this format:
+        # {
+        #     "status": "DONE",         # or "ERROR" if there was a general error and no files have been processed
+        #     "results": {
+        #         "file_name_0": {
+        #             "rmsd": {rmsd},
+        #             "error": 0        # or 1 if this file could not be processed due to an error (we can add more error types if needed)
+        #         },
+        #         "file_name_1": {
+        #             ...
+        #         },
+        #         ...
+        #     }
+        # }
+        # (if we use Python to write to `status.json`, we can just use `save_as_json.py` function from `API/scripts`)
+        # remember to delete `files/` directory after files are processed to save space
+        Thread(target=test, args=[task_id]).start()
         
     except Exception:
         return 1
