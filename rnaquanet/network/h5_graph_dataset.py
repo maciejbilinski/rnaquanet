ea4c574a141a70dd7765f26c126afe34d2ffb3d9
Maciej Bilinski <maciej.bilinski.programming@gmail.com>
2024-01-23 19:13:26 +0000

    Eval for comparison

--- rnaquanet/network/h5_graph_dataset.py
+++ rnaquanet/network/h5_graph_dataset.py
@@ -5,11 +5,12 @@ from torch_geometric.data import Data
 import random
 
 class H5GraphDataset(Dataset):
-    def __init__(self, filename, max_iterations = None, nan_replacement = 1000):
+    def __init__(self, filename, max_iterations = None, nan_replacement = 1000, return_key = False):
         self.filename = filename
         self.collection = None
         self.max_iterations = max_iterations
         self.nan = nan_replacement
+        self.return_key = return_key
 
     def shuffle(self):
         random.shuffle(self.keys)
@@ -19,12 +20,20 @@ class H5GraphDataset(Dataset):
             raise ValueError('To get length of dataset you have to first call __enter__ method or use "with" statement')
         value = self.collection[self.keys[idx]]
         if isinstance(value, h5py.Group):
-            return Data(
-                x=torch.nan_to_num(torch.tensor(value['x'][()]), nan=self.nan),
-                edge_index=torch.tensor(value['edge_index'][()]),
-                edge_attr=torch.tensor(value['edge_attr'][()]),
-                y=torch.tensor(value['y'][()]) if value.get('y') is not None else None
-            )
+            if self.return_key:
+                return self.keys[idx], Data(
+                    x=torch.nan_to_num(torch.tensor(value['x'][()]), nan=self.nan),
+                    edge_index=torch.tensor(value['edge_index'][()]),
+                    edge_attr=torch.tensor(value['edge_attr'][()]),
+                    y=torch.tensor(value['y'][()]) if value.get('y') is not None else None
+                )
+            else:
+                return Data(
+                    x=torch.nan_to_num(torch.tensor(value['x'][()]), nan=self.nan),
+                    edge_index=torch.tensor(value['edge_index'][()]),
+                    edge_attr=torch.tensor(value['edge_attr'][()]),
+                    y=torch.tensor(value['y'][()]) if value.get('y') is not None else None
+                )
         else:
             raise ValueError('Given H5 file does not contain only H5 Groups')
 
